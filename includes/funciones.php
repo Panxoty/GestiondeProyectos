2a7bcda74d266163e41eefead0c3d260dd908f88
Francisco Muñoz <panxitto40@gmail.com>
2024-02-19 20:52:41 -0300

    Se agrega bg a los estados

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,6 +1,7 @@
 <?php
 
-function debuguear($variable) : string {
+function debuguear($variable): string
+{
     echo "<pre>";
     var_dump($variable);
     echo "</pre>";
@@ -8,14 +9,17 @@ function debuguear($variable) : string {
 }
 
 // Escapa / Sanitizar el HTML
-function s($html) : string {
+function s($html): string
+{
     $s = htmlspecialchars($html);
     return $s;
 }
 
 // Función que revisa que el usuario este autenticado
-function isAuth() : void {
-    if(!isset($_SESSION['login'])) {
+function isAuth(): void
+{
+
+    if (!isset($_SESSION['login'])) {
         header('Location: /');
     }
 }
